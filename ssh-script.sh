ca3620decb32f6db8895bbe1b525eb48063e5b61
ikrom nur rohim <59647417+ikromnurrohim@users.noreply.github.com>
2022-12-08 17:41:21 +0700

    Add files via upload
    add file ssh-script.sh

---
+++ ssh-script.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+if [ "$1" == "" ]; then
+	echo "Usage: ./ssh-script.sh [path file key]"
+	echo "Example: ./ssh-script.sh idrsa_192.168.61.155_admin or ./ssh-script.sh /home/admin/idrsa_192.168.61.155_admin"
+else
+	counter=0
+        #looping each string that have _ (underscore)
+	for val in ${1//_/ }; do
+
+	   if [ "$counter" == "1" ]; then
+	       ip_address=$val
+	   fi
+
+	   if [ "$counter" == "2" ]; then
+	       username=$val
+	   fi
+
+	   #increment counter
+	   ((counter=counter+1))
+	done
+
+echo "ssh to server $ip_address"
+echo "using ssh key $1 with username $username"
+
+sudo ssh -i "$1" "$username"@"$ip_address"
+fi
+
+
